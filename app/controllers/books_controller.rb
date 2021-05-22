9c495a9c97366b636fcac8a5a9076147cfa0aee5
Fernando Eufrásio <fernando.epm1@gmail.com>
2021-05-22 13:05:12 -0300

    Create books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,19 @@
+# frozen_string_literal: true
+
 class BooksController < ApplicationController
+  before_action :set_book, only: :show
+
   def index
+    @q = Book.includes(:author).ransack(params[:q])
+
+    @books = @q.result(distinct: true).page(params[:page])
   end
 
-  def show
+  def show; end
+
+  private
+
+  def set_book
+    @book = Book.find(params[:id])
   end
 end
