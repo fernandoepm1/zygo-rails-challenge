ad6dc2a20cc4c90c09fbdbb9a2f921db661c6f4d
Fernando Eufrásio <fernando.epm1@gmail.com>
2021-05-22 11:17:44 -0300

    Create books controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root to: 'books#index'
+
+  get 'books/index'
+  get 'books/show'
   devise_for :users
   mount RailsAdmin::Engine => '/command_center', as: 'rails_admin'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
