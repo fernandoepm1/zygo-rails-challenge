c50eb8840a4b0125a757d23ef90becf4c327e805
Fernando Eufrásio <fernando.epm1@gmail.com>
2021-05-22 09:28:01 -0300

    Annotate models and spec files

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,12 @@
+# == Schema Information
+#
+# Table name: authors
+#
+#  id         :bigint           not null, primary key
+#  age        :integer
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Author < ApplicationRecord
 end
