653980e78c060a2f6f9a3f32bae4e13a3cd20134
Fernando Eufrásio <fernando.epm1@gmail.com>
2021-05-23 11:54:45 -0300

    Increment index method in books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,9 +4,15 @@ class BooksController < ApplicationController
   before_action :set_book, only: :show
 
   def index
-    @q = Book.includes(:author).ransack(params[:q])
+    @q = Book.ransack(params[:q])
 
-    @books = @q.result(distinct: true).page(params[:page])
+    books = @q.result(distinct: true)
+    load_authors && count_books(books) if books.any?
+
+    @books = @q.result(distinct: true)
+               .includes(:author)
+               .order(title: :asc)
+               .page(params[:page])
   end
 
   def show; end
@@ -15,5 +21,13 @@ class BooksController < ApplicationController
 
   def set_book
     @book = Book.find(params[:id])
+  end
+
+  def load_authors
+    @authors = Author.all
+  end
+
+  def count_books(books)
+    @books_count = books.count
   end
 end
