b72f872dc8058e022dcc1f8468dfe8db6d919b54
Fernando Eufrásio <fernando.epm1@gmail.com>
2021-05-24 09:16:45 -0300

    Update routes files

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   root to: 'books#index'
 
   get 'books/index'
-  get 'books/show'
+
   devise_for :users
   mount RailsAdmin::Engine => '/command_center', as: 'rails_admin'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
